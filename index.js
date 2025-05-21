b1f405372ba734ae348985d0bd58665e3bab2b67
pedrofeld <pedro.gael.feld@gmail.com>
2025-05-20 22:11:52 -0300

    chore: import do pacote odd e adição do type modules no package.json

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log('Hello, world! This is a test script.');
 
+import odd from 'odd';
+
+const oddNumbers = odd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log('Odd numbers from 1 to 10: ' + oddNumbers)
