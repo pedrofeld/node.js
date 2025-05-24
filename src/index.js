c0f7316640e30b075f617bc344bc74d05a3730fb
Pedro Feld <pedro.gael.feld@gmail.com>
2025-05-24 18:32:59 -0300

    feat: vínculo com a API Rick and Morty

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 console.log('Hello, world! This is a test script.');
 
 import odd from 'odd';
+import axios from 'axios';
 
 const oddNumbers = odd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log('Odd numbers from 1 to 10: ' + oddNumbers)
+
+axios
+    .get("https://rickandmortyapi.com/api/character")
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
