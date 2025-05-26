70c914e0ddb7e3f003dd31abb304e6e6a69f0818
Pedro Feld <pedro.gael.feld@gmail.com>
2025-05-26 20:42:22 -0300

    feat: função de buscar e criar livro(s)

--- src/livros.js
+++ src/livros.js
@@ -19,4 +19,31 @@ async function listarLivros() {
     }
 }
 
+async function buscarLivroPorId(id) {
+    try {
+        const result = await api.get(`/books/${id}`);
+        console.log(result.data);
+    } catch (error) {
+        console.error(`Erro ao buscar livro com ID ${id}:`, error);
+    }
+}
+
+async function criarLivro(livro) {
+    try {
+        const result = await api.post("/books", livro);
+        console.log("Livro criado:", result.data.data);
+    } catch (error) {
+        console.error("Erro ao criar livro:", error);
+    }
+}
+
 listarLivros();
+buscarLivroPorId("b1044037-70e4-42cf-9d9d-a8f80a1484b4");
+criarLivro({
+    title: "Novo Livro",
+    resume: "Esse é um resumo teste do livro teste",
+    totalPages: 150,
+    isFavorite: false, 
+    authorId: "a9715015-5d16-4e0b-a27d-8895ff183e3b"
+}
+)
