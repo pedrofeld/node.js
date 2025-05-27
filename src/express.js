0e98ca211571516c8045847d28d42c241c0c56bd
Pedro Feld <pedro.gael.feld@gmail.com>
2025-05-26 22:45:52 -0300

    feat: novas rotas na porta 3333

--- src/express.js
+++ src/express.js
@@ -5,6 +5,42 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/teste", (req, res) => {
+    console.log("Test route accessed");
+
+    res.send("Hello world response")
+})
+
+app.get("/hello", (req, res) => {
+    console.log("Hello route accessed");
+
+    res.send("Welcome to the Hello route!")
+})
+
+app.get("/about", (req, res) => {
+    console.log("About route accessed");
+
+    res.send({
+        nome: "Pedro Feld",
+        email: "pedro.gael.feld@gmail.com",
+        resume: "System Analyst and Developer",
+        age: 20,
+        skills: [
+            "JavaScript",
+            "TypeScript",
+            "HTML",
+            "CSS",
+            "Python",
+            "Node.js",
+            "React",
+            "Express",
+            "APIs REST",
+            "MySQL",
+            "PostgreSQL"
+        ]
+    })
+})
+
 const porta = process.env.PORT;
 
 app.listen(porta, () => {
