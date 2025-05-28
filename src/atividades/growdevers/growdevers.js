bece003a732caeeac6954684fedfe5b55e4ebf9a
Pedro Feld <pedro.gael.feld@gmail.com>
2025-05-27 21:33:01 -0300

    feat: adição do método post para criar um novo growdever

--- src/atividades/growdevers/growdevers.js
+++ src/atividades/growdevers/growdevers.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { growdevers } from './dados.js';
+import { randomUUID } from 'crypto'; 
 
 dotenv.config();
 
@@ -12,6 +13,28 @@ app.get("/growdevers", (req, res) => {
         ok: true,
         mensagem: "Growdevers listados com sucesso",
         dados: growdevers
+    });
+});
+
+app.post("/growdevers", (req, res) => {
+    // 1 etapa: entrada
+    const body = req.body;
+    const novoGrowdever = {
+        id: randomUUID(),
+        nome: body.nome,
+        email: body.email,
+        idade: body.idade,
+        matriculado: body.matriculado
+    };
+
+    // 2 etapa: processamento
+    growdevers.push(novoGrowdever);
+    
+    // 3 etapa: saída
+    res.status(201).send({
+        ok: true,
+        mensagem: "Growdever cadastrado com sucesso",
+        dados: novoGrowdever
     });
 });
 
